3153dac1cd42727cdfa7977a9efd21d44c72b391
irvingvasquez <jirvingvg@gmail.com>
2025-05-14 16:53:02 -0600

    complete the bib to csv script

--- scripts/bib2dataframe.py
+++ scripts/bib2dataframe.py
@@ -3,63 +3,31 @@ import sys
 import bibtexparser
 import pandas as pd
 
-def copy_file(source_file, destination_file):
+def copy_file(source_file, destination_file, entrytype):
     try:
         ## Papers
         with open(source_file) as bibtex_file:
             bibtex_str = bibtex_file.read()
 
         bib_database = bibtexparser.loads(bibtex_str)
-        #print(bib_database.entries)
 
-        products_df = pd.DataFrame(columns=['type', 'title', 'author', 'year', 'journal', 'volume', 'pages',\
-                                             'doi', 'url', 'note', 'issn', 'if', 'quartile', 'link', 'month_reported'])
+        matching_entries = [entry for entry in bib_database.entries if entry['ENTRYTYPE'] == entrytype]
+        
+        unique_keys = set([key for entry in matching_entries for key in entry.keys()])
+        print("Unique keys in matching entries:", unique_keys)
+        
+        products_df = pd.DataFrame(columns=list(unique_keys))
 
-        for entry in bib_database.entries:
-
+        for entry in matching_entries:
             try:
-                if entry['ENTRYTYPE'] == 'article':
-                    if entry['note'] == 'jcr':
-                        products_row = {'type': entry['ENTRYTYPE'],\
-                            'title': entry['title'],\
-                            'author': entry['author'],\
-                            'year': entry['year'],\
-                            'month': entry['month'],\
-                            'journal': entry['journal'],\
-                            'volume': entry['volume'],\
-                            'pages': entry['pages'],\
-                            'doi': entry['doi'],\
-                            'url': entry['url'],\
-                            'note': entry['note'],\
-                            'issn': entry['issn'],\
-                            'if': entry['if'],\
-                            'quartile': entry['quartile'],\
-                            'link': entry['link'],\
-                            'month_reported': entry['month_reported']}
-                        products_df = pd.concat([products_df, pd.DataFrame([products_row])], ignore_index=True)
+                products_row = {key:entry[key] for key in entry.keys()}
+                #print("products_row: ", products_row)
+                products_df = pd.concat([products_df, pd.DataFrame([products_row])], ignore_index=True)
 
-                    elif entry['note'] == 'conacyt':
-                        pass
-                    elif entry['note'] == 'divulgacion':
-                        pass
-                    else:
-                        pass
             except KeyError as e:
                 print("KeyError: ", e)
                 print(entry)
                 
-            if entry['ENTRYTYPE'] == 'inproceedings':
-                pass
-                
-            if entry['ENTRYTYPE'] == 'mastersthesis':
-                pass
-
-            if entry['ENTRYTYPE'] == 'phdthesis':
-                pass
-                
-            if entry['ENTRYTYPE'] == 'unpublished':
-                pass
-
         products_df.to_csv(destination_file, index=False)
 
         print("File copied successfully!")
@@ -67,10 +35,12 @@ def copy_file(source_file, destination_file):
         print("One or both files not found.")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python bib2dataframe.py <source_file.bib> <destination_file.csv>")
+    if len(sys.argv) != 4:
+        print("Usage: python bib2dataframe.py <source_file.bib> <destination_file.csv> <entrytype>")
+        print("Example: python bib2dataframe.py bibtex.bib bibtex.csv article")
     else:
         source_file = sys.argv[1]
         destination_file = sys.argv[2]
-        copy_file(source_file, destination_file)
+        entrytype = sys.argv[3]
+        copy_file(source_file, destination_file, entrytype)
 
